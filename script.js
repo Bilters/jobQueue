d5ef2dd106f7aa1690f9ef0b8f1a35d4f427b5d8
Sander Nijssen <nijssensander@gmail.com>
2017-01-18 22:45:46 +0100

    jobQueue System first tryout
    First setup of job queue system - pop-up for briefing input

---
+++ script.js
@@ -0,0 +1,33 @@
+// function nameSubmit() {
+//
+//     var newUser = document.getElementById('nameInput').value;
+//     if (newUser.length == 0) {
+//         alert('Please enter a real name');
+//         return;
+//     }
+//
+//     alert(newUser);
+// }
+// document.getElementById("nameInput")
+// .addEventListener("keyup", function(event) {
+//     event.preventDefault();
+//     if (event.keyCode == 13) {
+//         document.getElementById("submitBtn").click();
+//     }
+// });
+
+
+
+function submitJobPop() {
+
+    var TheURL = "form.html";
+    var PopWidth = "400";
+    var PopHeight = "400";
+    var PosTop = "200";
+    var PosLeft = "900";
+
+    var SettingSpopUpBox = "toolbar=no,menubar=no,scrollbars=no,resizable=yes,width=" + PopWidth + ",height=" + PopHeight + ",top=" + PosTop + ",left=" + PosLeft;
+    var PopUp = open(TheURL, "box", SettingSpopUpBox);
+}
+
+document.querySelector('#submitJob').addEventListener('click', submitJobPop);
