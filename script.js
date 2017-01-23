f1e3ebf50624ac3379587e72d683b49165fa78c1
Sander Nijssen <nijssensander@gmail.com>
2017-01-23 21:43:26 +0100

    MultiFunction
    More :bug: s found when evaluating form, it can’t re-submit

--- script.js
+++ script.js
@@ -17,31 +17,56 @@
 // });
 
 
-function checkProjectNumber() {
-    var dateGeneration = new Date();
-    var loProjectNumber = 120000;
-    var hiProjectNumber = dateGeneration.getYear() - 100 + "" + dateGeneration.getMonth() + 1 + "" + 99;
-    var projectNumber = document.jobCreation.projectNumber.value;
-    if (isNaN(projectNumber)) {
-        alert("Project numbers are numbers not letters");
-    } else {
-        if (projectNumber < loProjectNumber || projectNumber > hiProjectNumber) {
-            alert("This is not a project Number");
+function checkForm(){
+    function checkProjectNumber() {
+        var dateGeneration = new Date();
+        var loProjectNumber = 120000;
+        var hiProjectNumber = dateGeneration.getYear() - 100 + "" + dateGeneration.getMonth() + 1 + "" + 99;
+        var projectNumber = document.jobCreation.projectNumber.value;
+        if (isNaN(projectNumber)) {
+            alert("Project numbers are numbers not letters");
+        } else {
+            if (projectNumber < loProjectNumber || projectNumber > hiProjectNumber) {
+                alert("This is not a project Number");
+            }
         }
+        return false;
     }
+
+    function checkQuoteItem() {
+        if (document.jobCreation.quoteItem.value < 1) {
+            alert("A quote item should always be higher than one!");
+        }
+        return false;
+    }
+    return checkProjectNumber(), checkQuoteItem();
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('#submitJob').addEventListener('click', submitJobPop);
 function submitJobPop() {
     var TheURL = "modals/briefingForm.html";
-    var PopWidth = "400";
-    var PopHeight = "400";
+    var PopWidth = "900";
+    var PopHeight = "600";
     var PosTop = "200";
-    var PosLeft = "900";
+    var PosLeft = "800";
 
     var SettingSpopUpBox = "toolbar=no,menubar=no,scrollbars=no,resizable=yes,width=" + PopWidth + ",height=" + PopHeight + ",top=" + PosTop + ",left=" + PosLeft;
     var PopUp = open(TheURL, "box", SettingSpopUpBox);
 }
-
-document.querySelector('#submitForm').addEventListener('click', checkProjectNumber);
-document.querySelector('#submitJob').addEventListener('click', submitJobPop);
