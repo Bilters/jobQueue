f9d21c182d03723202320787362103e907770a03
Sander Nijssen <nijssensander@gmail.com>
2017-01-25 22:13:03 +0100

    Added returns

--- modals/briefingCheck.js
+++ modals/briefingCheck.js
@@ -6,17 +6,27 @@ function checkForm() {
         var projectNumber = document.jobCreation.projectNumber.value;
         if (isNaN(projectNumber)) {
             alert("Project numbers are numbers not letters");
+            return false;
         } else {
             if (projectNumber < loProjectNumber || projectNumber > hiProjectNumber) {
                 alert("This is not a project Number");
+                return false;
             }
         }
-        return false;
+        return;
     }
 
     function checkQuoteItem() {
         if (document.jobCreation.quoteItem.value < 1) {
             alert("A quote item should always be higher than one!");
+            return false;
+        } else if(isNaN(document.jobCreation.quoteItem.value)) {
+            alert("QuoteItems are numbers");
+            return false;
         }
+        return;
     }
+
+    checkProjectNumber();
+    checkQuoteItem();
 }
