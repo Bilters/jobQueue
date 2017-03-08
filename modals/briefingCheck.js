f1fd5a59b82ebf6b983a99c5406af44290c3deb4
Sander Nijssen <nijssensander@gmail.com>
2017-03-08 22:22:10 +0100

    Fixed functionality BriefingFormCheck

--- modals/briefingCheck.js
+++ modals/briefingCheck.js
@@ -1,32 +1,37 @@
-function checkForm() {
-    function checkProjectNumber() {
-        var dateGeneration = new Date();
-        var loProjectNumber = 120000;
-        var hiProjectNumber = dateGeneration.getYear() - 100 + "" + dateGeneration.getMonth() + 1 + "" + 99;
-        var projectNumber = document.jobCreation.projectNumber.value;
-        if (isNaN(projectNumber)) {
-            alert("Project numbers are numbers not letters");
-            return false;
-        } else {
-            if (projectNumber < loProjectNumber || projectNumber > hiProjectNumber) {
-                alert("This is not a project Number");
-                return false;
-            }
-        }
-        return;
-    }
+function validate() {
+    return (checkProjectNumber() && checkQuoteItem());
+}
 
-    function checkQuoteItem() {
-        if (document.jobCreation.quoteItem.value < 1) {
-            alert("A quote item should always be higher than one!");
-            return false;
-        } else if (isNaN(document.jobCreation.quoteItem.value)) {
-            alert("QuoteItems are numbers");
-            return false;
-        }
-        return;
-    }
+function checkProjectNumber() {
+    var isValid = true;
+    var projectNumber = document.jobCreation.projectNumber.value;
+    var dateGeneration = new Date();
+    var loProjectNumber = 120000;
+    var hiProjectNumber = dateGeneration.getYear() - 100 + "" + dateGeneration.getMonth() + 1 + "" + 99;
 
-    checkProjectNumber();
-    checkQuoteItem();
+    if (isNaN(projectNumber)) {
+        isValid = false;
+        alert("Project numbers are numbers not letters");
+    } else
+    if (projectNumber < loProjectNumber || projectNumber > hiProjectNumber) {
+        isValid = false;
+        alert("This is not a project Number");
+    }
+    return isValid;
+}
+
+function checkQuoteItem() {
+    var quoteItem = document.jobCreation.quoteItem.value;
+    var isValid = true;
+
+    if (quoteItem < 1) {
+        isValid = false;
+        alert("A quote item should always be higher than one!");
+
+    } else
+    if (isNaN(quoteItem)) {
+        isValid = false;
+        alert("QuoteItems are numbers not letters");
+    }
+    return isValid;
 }
