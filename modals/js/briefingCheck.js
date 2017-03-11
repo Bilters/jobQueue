acaec30f59544d5200b1f81af447ca26794e6cac
Sander Nijssen <nijssensander@gmail.com>
2017-03-11 01:33:21 +0100

    BriefingForm styling & more functionality
    Projectnumbers now completely fixed

--- modals/js/briefingCheck.js
+++ modals/js/briefingCheck.js
@@ -6,8 +6,12 @@ function checkProjectNumber() {
     var isValid = true;
     var projectNumber = document.jobCreation.projectNumber.value;
     var dateGeneration = new Date();
+    var month = dateGeneration.getMonth() + 1;
+    if (month < 9) {
+        month = "0" + month;
+    }
     var loProjectNumber = 120000;
-    var hiProjectNumber = dateGeneration.getYear() - 100 + "" + dateGeneration.getMonth() + 1 + "" + 99;
+    var hiProjectNumber = dateGeneration.getYear() - 100 + "" + month + "" + 99;
 
     if (isNaN(projectNumber)) {
         isValid = false;
@@ -15,7 +19,16 @@ function checkProjectNumber() {
     } else
     if (projectNumber < loProjectNumber || projectNumber > hiProjectNumber) {
         isValid = false;
-        alert("This is not a project Number");
+        alert("This is not a project number");
+    } else
+    if (projectNumber[2] > 1) {
+        isValid = false;
+        alert("This is not a project number");
+    } else
+    if (projectNumber[2] + "" + projectNumber[3] > 12) {
+        isValid = false;
+        alert("This is not a project number");
+
     }
     return isValid;
 }
